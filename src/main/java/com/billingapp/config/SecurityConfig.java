1300b6e03a57762c44d833042a8a921eefe2dfcf
yadavmangesh07 <mangeshyadav8736@gmail.com>
2026-03-04 17:36:21 +0530

    feat: Update CORS configuration to allow access from the client backend URL

--- src/main/java/com/billingapp/config/SecurityConfig.java
+++ src/main/java/com/billingapp/config/SecurityConfig.java
@@ -64,7 +64,7 @@ public class SecurityConfig {
     @Bean
     public UrlBasedCorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173","https://client-backend-zeta.vercel.app/login"));
+        config.setAllowedOrigins(List.of("http://localhost:5173","https://client-backend-zeta.vercel.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS","PATCH"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
